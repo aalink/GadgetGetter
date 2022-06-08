9f329bd763efe4a7125be9603236f8f5fe5a6438
Jessie Doherty <zhuzhu930@yahoo.com>
2022-06-08 17:36:30 -0500

    modified database files

--- seeds/seed.js
+++ seeds/seed.js
@@ -7,17 +7,24 @@ const deviceData = require('./deviceData.json');
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
 
-  const users = await User.bulkCreate(userData, {
+  await User.bulkCreate(userData, {
     individualHooks: true,
     returning: true,
   });
 
-  for (const device of deviceData) {
-    await Device.create({
-      ...device,
-      user_id: users[Math.floor(Math.random() * users.length)].id,
-    });
-  }
+  await Device.bulkCreate(deviceData, {
+    returning: true,
+  })
+
+
+  //Note: I don't think we can use random number to assign the user_id, as some devices are available, some are not. I've hard coded some of the user_id in the database
+
+  // for (const device of deviceData) {
+  //   await Device.create({
+  //     ...device,
+  //     user_id: users[Math.floor(Math.random() * users.length)].id,
+  //   });
+  // }
 
   process.exit(0);
 };
